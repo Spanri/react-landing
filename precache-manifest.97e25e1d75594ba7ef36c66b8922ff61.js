cad01721f6395f4c1a8901b2129cd9f0faf5898d
Spanri <kozlova9v@mail.ru>
2019-08-04 05:26:44 +0300

    Updates

---
+++ precache-manifest.97e25e1d75594ba7ef36c66b8922ff61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3d4b1f5c38712acbf82d4668f31451",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "fe674a838540b320235d",
+    "url": "/react/static/css/main.7ddfc63a.chunk.css"
+  },
+  {
+    "revision": "a34a5d3b6008fcd267d6",
+    "url": "/react/static/js/2.41934161.chunk.js"
+  },
+  {
+    "revision": "fe674a838540b320235d",
+    "url": "/react/static/js/main.88442e49.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "7584c9b044c6638575273a2ad231be5f",
+    "url": "/react/static/media/fireplace.7584c9b0.jpg"
+  }
+]);
